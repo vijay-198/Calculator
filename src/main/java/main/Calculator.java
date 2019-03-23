94f8c1776e5b2dd40b1bd3647b3ae1522659e654
chaitanya <chaitanya@chaitanya-PC>
2019-03-23 11:40:57 +0530

    modify

--- src/main/java/main/Calculator.java
+++ src/main/java/main/Calculator.java
@@ -5,7 +5,7 @@ public class Calculator {
     public static void main(String[] args)
     {
         System.out.println("Enter first and second number:");
-        Scanner inp= new Scanner(System.in);
+        Scanner inp = new Scanner(System.in);
         int num1,num2;
         num1 = inp.nextInt();
         num2 = inp.nextInt();
@@ -27,7 +27,7 @@ public class Calculator {
             System.out.println(div( num1,num2));
             break;
             default:
-                System.out.println("Illigal Operation");
+                System.out.println("Illegal Operation");
 
 
         }
@@ -39,25 +39,28 @@ public class Calculator {
     {
     	int result=0;
 //        write your code here 
-    	
-        return result;
+    	result = x+y;
+    	return result;
     }
     public static int sub(int x, int y)
     {
     	int result=0;
 //        write your code here
+    	result = x-y;
         return result;
     }
     public static int mult(int x, int y)
     {
     	int result=0;
-//      write your code here        
+//      write your code here
+    	result = x*y;
     	return result;
     }
     public static int div(int x, int y)
     {
     	int result=0;
 //      write your code here
+    	result = x/y;
     	return result;
     }
 
